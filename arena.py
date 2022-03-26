5bec99ba7c2f08dab97c9c6d1cb1b13cb88dd6c4
john <fortyrunner@gmail.com>
2022-03-26 15:35:44 +0000

    Print member list as CSV

--- arena.py
+++ arena.py
@@ -1,9 +1,21 @@
-from power_of_10 import athletes, exceptions
+from power_of_10 import athletes, coaches, exceptions
 
-arena = athletes.search_athletes(club="Arena 80")
 
-print(" There are ", len(arena), " Arena 80 athletes")
+def get_arena_members():
+    members = athletes.search_athletes(club="Arena 80")
+    members.sort(key=lambda item: int(item.get("athlete_id")))
+    return members
 
+def print_member_list(members):
+    print(" There are ", len(members), " Arena 80 athletes")
+    print("id,firstname,surname,age,sex")
+    for athlete in members:
+        print(athlete["athlete_id"], ",", athlete["firstname"], ",", athlete["surname"], ",", athlete["sex"], ",",
+              athlete["track"])
+
+
+members = get_arena_members()
+print_member_list(members)
 
 
 
